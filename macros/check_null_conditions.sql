e26cfcad66dda8c1ae443f0659315c01ee6ec8e4
pradeeps <pradeepdhanola100l@gmail.com>
2026-07-17 00:38:30 +0530

    clean slate: remove old vishal models/macros and test files

--- macros/check_null_conditions.sql
+++
@@ -1,7 +0,0 @@
-{% macro check_null_conditions(column_list) %}
-    {% set conditions = [] %}
-    {% for col in column_list %}
-        {% do conditions.append(col ~ ' IS NULL') %}
-    {% endfor %}
-    ({{ conditions | join(' OR ') }})
-{% endmacro %}
